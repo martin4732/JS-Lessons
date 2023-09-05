0560292b321ad77aaa0cc0889ad978ffefe947a4
martin4732 <dokumartin33@gmail.com>
2023-09-05 07:16:58 +0000

    new changes

--- JS/SlideShow.js
+++ JS/SlideShow.js
@@ -1,40 +1,12 @@
-(function() {
-    
-        const image = document.getElementById('images');
-        const next = document.getElementById('next');
-        const pre = document.getElementById('previous');
-        const myimages = ['1.png', '2.png', '3.png', '4.png', '5.png', '6.png', '7.png'];
-        let currntImage = 0;
 
-        next.addEventListener('click', function() { 
-            currntImage++;
-            if (currntImage > myimages.length - 1) {
-                //sets the image to always be the last image without moving to the first image
-                //currntImage = myimages.length - 1;
-            //returns image back to the first image after the 
-                currntImage = 0;
-            } 
-            
-            image.src = myimages[currntImage];
+const nextButton = document.getElementById('next');
+const previousButton = document.getElementById('previous');
+let currentImageCount = 0;
+var arrayOfImages =['../IMAGES/img1.png', '../IMAGES/img2.png', '../IMAGES/img3.png', '../IMAGES/img4.png', '../IMAGES/img5.png'];
 
-        })
-        pre.addEventListener('click', function() {
-            if (currntImage === 0) {
-                image.src = myimages[currntImage];
-            } else {
-                currntImage--;
-                image.src = myimages[currntImage];
-            }
-        })
-} () )
-
-
-
-/*
-var currntImage = 0;
-document.getElementById('next').onclick = nextPhoto
+nextButton.onclick = nextPhoto;
 
 function nextPhoto() {
-    currntImage++;
-    document.getElementById('images').src = myimages[currntImage];
-} */
+    currentImageCount++;
+    document.getElementById('image').src = arrayOfImages[currentImageCount]
+}
