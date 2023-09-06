f0eaa7de61a945bb0767dc411d451a59b8b8afb5
martin4732 <dokumartin33@gmail.com>
2023-09-06 08:33:00 +0000

    Commiting changes in all folders

--- JS/SlideShow.js
+++ JS/SlideShow.js
@@ -1,12 +1,21 @@
-
 const nextButton = document.getElementById('next');
 const previousButton = document.getElementById('previous');
+const myImages = document.getElementById('image')
 let currentImageCount = 0;
 var arrayOfImages =['../IMAGES/img1.png', '../IMAGES/img2.png', '../IMAGES/img3.png', '../IMAGES/img4.png', '../IMAGES/img5.png'];
 
-nextButton.onclick = nextPhoto;
-
-function nextPhoto() {
+nextButton.addEventListener('click', function(){
     currentImageCount++;
-    document.getElementById('image').src = arrayOfImages[currentImageCount]
-}
+    if (currentImageCount > arrayOfImages.length - 1) {
+        currentImageCount = 0;
+    }
+    myImages.src = arrayOfImages[currentImageCount];
+})
+
+previousButton.addEventListener('click', function(){
+    currentImageCount--;
+    if (currentImageCount < 0) {
+        currentImageCount = arrayOfImages.length -1;
+    }
+    myImages.src = arrayOfImages[currentImageCount]
+})
---
+++ JS/SlideshowCrosseffect.js
@@ -0,0 +1,38 @@
+var container = document.getElementById('content');
+const nextButton = document.getElementById('next');
+const previousButton = document.getElementById('previous');
+var currentImageCount = 0;
+const imagesInArray = ['../IMAGES/img1.png', '../IMAGES/img2.png', '../IMAGES/img3.png', '../IMAGES/img4.png', 
+'../IMAGES/img5.png',];
+
+
+nextButton.addEventListener('click', function() {
+    currentImageCount++;
+    if (currentImageCount > imagesInArray.length - 1) {
+        currentImageCount = 0;
+    }
+    creatingElementsAndRemovindChild();
+    
+})
+
+previousButton.addEventListener('click', function(){
+    currentImageCount--;
+    if (currentImageCount < 0) {
+        currentImageCount =  imagesInArray.length -1;
+    }
+    creatingElementsAndRemovindChild();
+})
+
+
+function creatingElementsAndRemovindChild() {
+
+    var newImageEelement = document.createElement('img');
+    newImageEelement.src = imagesInArray[currentImageCount];
+    newImageEelement.className = "fadeinImg";
+    container.appendChild(newImageEelement);
+
+    if (container.children.length > 2) {
+        container.removeChild(container.children[0])
+    }
+
+}
